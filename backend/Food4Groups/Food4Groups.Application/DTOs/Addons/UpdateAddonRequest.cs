4f7f5e218e134c87850ce156f4adb1090932d4c3
kmlynek <kmlynek65@gmail.com>
2026-05-27 00:13:57 +0200

    set default isActive to true for Addon and Dish update DTOs

--- backend/Food4Groups/Food4Groups.Application/DTOs/Addons/UpdateAddonRequest.cs
+++ backend/Food4Groups/Food4Groups.Application/DTOs/Addons/UpdateAddonRequest.cs
@@ -4,5 +4,5 @@ public class UpdateAddonRequest
 {
     public required string Name { get; set; }
     public string? Description { get; set; }
-    public bool IsActive { get; set; }
+    public bool IsActive { get; set; } =  true;
 }
--- backend/Food4Groups/Food4Groups.Application/DTOs/Dishes/UpdateDishRequest.cs
+++ backend/Food4Groups/Food4Groups.Application/DTOs/Dishes/UpdateDishRequest.cs
@@ -4,5 +4,5 @@ public class UpdateDishRequest
 {
     public required string Name { get; set; }
     public string? Description { get; set; }
-    public bool IsActive { get; set; }
+    public bool IsActive { get; set; } =  true;
 }
