b641cc1d5893efe1045c8f45472597a28a623069
kmlynek <kmlynek65@gmail.com>
2026-05-26 19:55:09 +0200

    update .gitignore file

--- backend/Food4Groups/Food4Groups.Api/Extensions/IdentitySeeder.cs
+++ backend/Food4Groups/Food4Groups.Api/Extensions/IdentitySeeder.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Food4Groups.Api.Extensions;
 
-//Inicjalne wypełnienie danych IDentity wywoływane przy starcie aplikacji
+// Inicjalne wypełnienie danych IDentity wywoływane przy starcie aplikacji
 public static class IdentitySeeder
 {
     private static readonly string[] Roles =
@@ -14,10 +14,10 @@ public static class IdentitySeeder
         "User"
     ];
     
-    //Tworzy role oraz konto admina, IServiceProvider - interfejs odpowiedzialny za pobieranie instancji  
+    // Tworzy role oraz konto admina, IServiceProvider - interfejs odpowiedzialny za pobieranie instancji  
     public static async Task SeedIdentityAsync(this IServiceProvider services)
     {
-        //Tworzy scope aby pobrać zależności z DI  (w nim DI jest dostępne), 'using' - po zakończeniu pracy cała szuflada (scope) zostanie zamknięta, a obiekty usuniete z pamieci 
+        // Tworzy scope aby pobrać zależności z DI  (w nim DI jest dostępne), 'using' - po zakończeniu pracy cała szuflada (scope) zostanie zamknięta, a obiekty usuniete z pamieci 
         using var scope = services.CreateScope();
         
         var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
@@ -32,14 +32,14 @@ public static class IdentitySeeder
             }
         }
         
-        //Seed konta admina
+        // Seed konta admina
         const string adminEmail = "admin@food4groups.com";
         const string adminPassword = "Admin123!";
         
         var admin  = await userManager.FindByNameAsync(adminEmail);
         if (admin == null)
         {
-            //Tworzy konto admina
+            // Tworzy konto admina
             admin = new IdentityUser
             {
                 UserName = adminEmail,
@@ -48,7 +48,7 @@ public static class IdentitySeeder
             };
             
             var createResult = await userManager.CreateAsync(admin, adminPassword);
-            //Jesli utworzenie sie powiodlo, przypisuje rolę Admin
+            // Jesli utworzenie sie powiodlo, przypisuje rolę Admin
             if (createResult.Succeeded)
             {
                 await userManager.AddToRoleAsync(admin, "Admin");
