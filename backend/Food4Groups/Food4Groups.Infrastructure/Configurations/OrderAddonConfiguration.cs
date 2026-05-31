aa989f3574570b5cd947371d65b4b6376e1a2285
kmlynek <kmlynek65@gmail.com>
2026-06-01 00:18:27 +0200

    add OrderAddon EF Core configuration

---
+++ backend/Food4Groups/Food4Groups.Infrastructure/Configurations/OrderAddonConfiguration.cs
@@ -0,0 +1,31 @@
+using Food4Groups.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Food4Groups.Infrastructure.Configurations;
+
+public class OrderAddonConfiguration : IEntityTypeConfiguration<OrderAddon>
+{
+    public void Configure(EntityTypeBuilder<OrderAddon> builder)
+    {
+        builder.ToTable("OrderAddons");
+        
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+        
+        builder.HasOne(x => x.Order)
+            .WithMany()
+            .HasForeignKey(x => x.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Addon)
+            .WithMany()
+            .HasForeignKey(x => x.AddonId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(x => new { x.OrderId, x.AddonId })
+            .IsUnique();
+    }
+}
