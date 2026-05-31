7cc32d7dd480d3a63cdfcaf1a1b1ff3c2224b0d4
kmlynek <kmlynek65@gmail.com>
2026-06-01 00:12:36 +0200

    add Order EF Core configuration

---
+++ backend/Food4Groups/Food4Groups.Infrastructure/Configurations/OrderConfiguration.cs
@@ -0,0 +1,41 @@
+using Food4Groups.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Food4Groups.Infrastructure.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.ToTable("Orders");
+        
+        builder.HasKey(x => x.Id);
+        
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+
+        builder.HasOne(x => x.GroupMember)
+            .WithMany()
+            .HasForeignKey(x => x.GroupMemberId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.MenuDay)
+            .WithMany()
+            .HasForeignKey(x => x.MenuDayId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.Dish)
+            .WithMany()
+            .HasForeignKey(x => x.DishId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.OrderStatus)
+            .WithMany()
+            .HasForeignKey(x => x.OrderStatusId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(x => new { x.GroupMemberId, x.MenuDayId })
+            .IsUnique();
+    }
+}
