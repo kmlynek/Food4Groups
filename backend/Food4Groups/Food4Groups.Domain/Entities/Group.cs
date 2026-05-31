197b6a85c05514957fa25077912c013947b8da2f
kmlynek <kmlynek65@gmail.com>
2026-05-31 13:54:26 +0200

    update Group and Package entities

--- backend/Food4Groups/Food4Groups.Domain/Entities/Group.cs
+++ backend/Food4Groups/Food4Groups.Domain/Entities/Group.cs
@@ -3,7 +3,10 @@ namespace Food4Groups.Domain.Entities;
 public class Group
 {
     public Guid Id { get; set; }
+    public Guid CateringCompanyId { get; set; }
     public required string Name { get; set; }
     public int MemberCount { get; set; }
     public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
+    
+    public CateringCompany? CateringCompany { get; set; }
 }
--- backend/Food4Groups/Food4Groups.Domain/Entities/Package.cs
+++ backend/Food4Groups/Food4Groups.Domain/Entities/Package.cs
@@ -4,6 +4,10 @@ public class Package
 {
     public Guid Id { get; set; }
     public required string Name { get; set; }
+    public Guid CateringCompanyId { get; set; }
     public decimal PricePerPerson { get; set; }
     public bool IsActive { get; set; } = true;
+    public DateTime Created { get; set; }
+    
+    public CateringCompany? CateringCompany { get; set; }
 }
