3dcf3c79c2168dfdcbce7ec503647201e4c5b93b
kmlynek <kmlynek65@gmail.com>
2026-05-31 22:22:49 +0200

    add MenuDay EF Core configuration

---
+++ backend/Food4Groups/Food4Groups.Infrastructure/Configurations/MenuDayConfiguration.cs
@@ -0,0 +1,32 @@
+using Food4Groups.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Food4Groups.Infrastructure.Configurations;
+
+public class MenuDayConfiguration : IEntityTypeConfiguration<MenuDay>
+{
+    public void Configure(EntityTypeBuilder<MenuDay> builder)
+    {
+        builder.ToTable("MenuDays");
+        
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.MenuDate)
+            .IsRequired();
+        
+        builder.Property(x => x.IsActive)
+            .IsRequired();
+        
+        builder.Property(x => x.CreatedAt)
+            .IsRequired();
+        
+        builder.HasOne(x => x.MenuPeriod)
+            .WithMany()
+            .HasForeignKey(x => x.MenuPeriodId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasIndex((x => new { x.MenuPeriodId, x.MenuDate }));
+    }
+    
+}
