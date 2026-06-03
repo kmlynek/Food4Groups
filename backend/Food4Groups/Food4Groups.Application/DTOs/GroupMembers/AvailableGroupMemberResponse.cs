d5e3ce45f0525d2a9ba740db5f5377008368c72e
kmlynek <kmlynek65@gmail.com>
2026-06-03 14:10:29 +0200

    add AvailableGroupMember response DTO

---
+++ backend/Food4Groups/Food4Groups.Application/DTOs/GroupMembers/AvailableGroupMemberResponse.cs
@@ -0,0 +1,7 @@
+namespace Food4Groups.Application.DTOs.GroupMembers;
+
+public class AvailableGroupMemberResponse
+{
+    public required string Id { get; set; }
+    public string? Email { get; set; }
+}
