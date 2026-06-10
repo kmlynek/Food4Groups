64e60edab1bcd40c30e5a656394ae68f9b812f7a
kmlynek <kmlynek65@gmail.com>
2026-06-10 20:19:16 +0200

    remove common fields from Dish entity

--- backend/Food4Groups/Food4Groups.Domain/Entities/Dish.cs
+++ backend/Food4Groups/Food4Groups.Domain/Entities/Dish.cs
@@ -1,10 +1,10 @@
+using Food4Groups.Domain.Common;
+
 namespace Food4Groups.Domain.Entities;
 
-public class Dish
+public class Dish : BaseEntity
 {
-    public Guid Id { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
     public bool IsActive { get; set; } = true;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
